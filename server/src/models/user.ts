3854a0d3b8e77bb6e2c04cb69841c203efaad063
Haralds Treilons <haralds_treilons@inbox.lv>
2022-12-28 21:40:19 +0200

    small update

---
+++ server/src/models/user.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: {
+    User: {
+      type: Number,
+      default: 1000,
+    },
+    Admin: Number,
+  },
+  hasPaid: {
+    type: Boolean,
+    default: false,
+  },
+}, { timestamps: true})
+
+export const UserModel = mongoose.model('user', userSchema, 'user');
